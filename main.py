fef28cc3df4a2abae9a1aeda5ce90d2519fdb9a3
MSameer192 <sameer192.official@gmail.com>
2023-08-26 07:58:01 +0500

    basics

---
+++ main.py
@@ -0,0 +1,168 @@
+# print('Hello World')
+#
+# age = 20
+# age = 40.1
+# first_name = 'Sameer'
+# is_Online = True
+# name = input('What is your name ')
+# print('Hello ' + name)
+# print(age)
+# print(first_name, is_Online)
+
+# ========================================
+
+# birth_year = input('Enter your birthday year: ')
+# age = 2023 - int(birth_year)
+
+# print(age)
+
+# ========================================
+
+# int()
+# float()
+# bool()
+# str()3
+
+# first = input('First: ')
+# second = input('Second: ')
+# sum = float(first) + float(second)
+
+# print("Total: " + str(sum))
+
+# ========================================
+
+# course = 'Python for Sameer'
+
+# print(course.upper())
+# print(course.lower())
+
+# print(course.find('for')) #tocheckindex
+# print('Python' in course) #tocheckexist true or false
+# print(course.replace('Sameer', 'Memon'))
+# print(course)
+
+# ========================================
+# Arthmethic
+# print( 10 ** 3) #topower
+
+# Augementic
+# w = 10
+# w += 3
+#
+# x = 10
+# x -= 3
+#
+# y = 10
+# y /= 3
+#
+# z = 10
+# z *= 3
+#
+# print(w, x, y, z)
+
+# ========================================
+
+# Operator Precedence
+
+# x = 10 + 3 * 2
+# x = (10 + 3) * 2
+
+# ========================================
+
+# Comparision Operator
+
+# x = 3 >= 2
+# x = 3 <= 2
+# x = 3 == 2
+# x 3 != 2
+#
+# print(x)
+
+# ========================================
+
+#  Logical operator
+
+# price = 25
+# print(price > 10 and price < 30) -- and operator
+# print(price > 30 or price < 30) # or operator
+# print( not price > 30) # not operator
+
+# ========================================
+
+# if else
+
+# temperature = int(input('temperature '))
+#
+# if temperature > 30:
+#     print("It's a hot day")
+# elif temperature > 20:
+#     print("Its a nice day")
+# elif temperature > 10:
+#     print('Its a bit cold')
+# else:
+#     print('Its cold')
+#
+# print('done')
+
+# ========================================
+
+# While Loops
+
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i = i + 1
+
+# -----------------------
+
+# numbers = [1,2,3,4,5]
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i + 1
+
+
+# ========================================
+
+# list
+
+# name = ['sameer', 'memon', 'mosh']
+# print(name[-1])
+# print(name[-2])
+# name[0] = 'sameer memon'
+# print(name[0:2])
+
+# list methods
+
+# numbers = [1,2,3,4,5]
+# numbers.append(6) #toadd number in last
+# numbers.insert(0, 3) #toadd number in first
+# numbers.remove(3)
+# numbers.clear()
+# print(1 in numbers)
+# print(len(numbers))
+# print(numbers)
+
+# ========================================
+
+# numbers = [1,2,3,4,5]
+# for item in numbers:
+#     print(item)
+# print(numbers)
+
+# range oject function
+
+# numbers = range(5, 15, 3)
+# numbers = range(5, 15) #5 to 15 print
+# numbers = range(5, 50, 3) #5 to 15 print with 3 diff
+#
+# for number in numbers:
+# for number in range(5):
+#
+#     print(number)
+
+# ========================================
+
+# Tupple is imutable mean not changeable
+
+# numbers = (1,2,3,4,5)
